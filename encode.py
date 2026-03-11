87c51938f9806d435a152f7f1bef83132f7f2bcd
Quazi Heider <quaziheider@uvic.ca>
2026-03-11 00:26:43 -0700

    Added support for converting multiple files at once

--- encode.py
+++ encode.py
@@ -29,10 +29,17 @@ class ConverterUI(QWidget):
         self.label4.setAlignment(Qt.AlignmentFlag.AlignHCenter)
         self.label4.setStyleSheet("color: gray; font-size: 12px; font-weight: bold")
 
-        self.select_btn = QPushButton("Select Video")
+        self.label5 = QLabel("No destination selected")
+        self.label5.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label5.setStyleSheet("font-size: 18px; font-weight: bold")
+
+        self.select_btn = QPushButton("Select Videos")
         self.select_btn.setFixedSize(900, 70)
+        self.dest_btn = QPushButton("Select Destination Folder")
+        self.dest_btn.setFixedSize(900,70)
         self.convert_btn = QPushButton("Convert to MP4")
         self.convert_btn.setFixedSize(900, 70)
+        self.convert_btn.setEnabled(False)
 
         layout = QVBoxLayout()
         layout.addStretch(1)          # space above title
@@ -44,48 +51,66 @@ class ConverterUI(QWidget):
         layout.addStretch(1)          # space below
 
         layout.addWidget(self.label2) # No file selected
-        layout.addStretch(2)          # space below
+        layout.addStretch(0)          # space below
+        layout.addWidget(self.label5)
+        layout.addStretch(1)
 
         layout.addWidget(self.select_btn)
+        layout.addWidget(self.dest_btn)
         layout.addWidget(self.convert_btn)
+
 
         self.setLayout(layout)
 
         self.select_btn.clicked.connect(self.select_video)
+        self.dest_btn.clicked.connect(self.select_destination)
         self.convert_btn.clicked.connect(self.convert_video)
 
-        self.input_file = None
+        self.input_files = None
 
     def select_video(self):
-        file, _ = QFileDialog.getOpenFileName(self, "Select Video")
-        if file:
-            self.input_file = file
-            self.label2.setText(file)
-            self.label5 = QLabel("Select destination folder")
+        files, _ = QFileDialog.getOpenFileNames(self, "Select Videos")
+        if files:
+            self.input_files = files
+            filenames = [os.path.basename(file) for file in files]
+            label_text = ""
+            if len(filenames) > 5:
+                n = len(filenames)
+                label_text = str(n) + " files selected"
+            else: 
+                for filename in filenames:
+                    label_text = label_text + filename + ', '
+    
+            self.label2.setText(label_text)
+            self.label2.setStyleSheet("font-size: 18px; font-weight: bold")
+           
 
     def select_destination(self):
         folder = QFileDialog.getExistingDirectory(self, "Select destination folder")
         if folder:
             self.output_folder = folder
-    
+            self.label5.setText(os.path.basename(folder))
+            self.convert_btn.setEnabled(True)
 
     def convert_video(self):
-        if not self.input_file:
+        if not self.input_files:
             self.label2.setText("Select a file first")
             self.label2.setStyleSheet("color: red; font-size: 18px; font-weight: bold")
             return
         
         try:
-            self.label2.setText("Converting...")
+            
             import subprocess
-            output = self.input_file + ".mp4"
-            subprocess.run([
-                "ffmpeg",
-                 "-i", self.input_file,
-                "-c:v", "libx264",
-                "-c:a", "aac",
-                output
-            ], check = True)
+            for file in self.input_files:
+                output_filename = os.path.basename(file[:-4]) + ".mp4"
+                output = os.path.join(self.output_folder, output_filename)
+                subprocess.run(["ffmpeg", "-i", file, "-c:v", "libx264", "-c:a", "aac", output], check = True)
+                self.label2.setText("Converting " + output_filename[:-4])
+                self.label2.setStyleSheet("color: yellow; font-size: 18px; font-weight: bold")
+                QApplication.processEvents()
+
+            self.label2.setText("Conversion completed successfully.")
+            self.label2.setStyleSheet("color: green; font-size: 18px; font-weight: bold")
         except Exception as e:
             msg = QMessageBox()
             msg.setIcon(QMessageBox.Icon.Critical)
@@ -93,9 +118,10 @@ class ConverterUI(QWidget):
             msg.setText("Something went wrong during conversion.")
             msg.setInformativeText(str(e))
             msg.exec()
+            self.label2.setText("Conversion unsuccessful.")
+            self.label2.setStyleSheet("color: red; font-size: 18px; font-weight: bold")
 
-        self.label2.setText("Conversion completed successfully.")
-        self.label2.setStyleSheet("color: green; font-size: 18px; font-weight: bold")
+        
 
 
 app = QApplication(sys.argv)
